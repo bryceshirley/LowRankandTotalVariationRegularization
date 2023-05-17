f2e802f9e70b52e8098e566a368c5cd1b3d191de
~bryceshirley <~bryce.shirley@stfc.ac.uk>
2023-05-17 18:12:02 +0100

    Parameter changes

--- TestFunctions.m
+++ TestFunctions.m
@@ -1,10 +1,10 @@
 %% Generate Image/Corrupted Image
 
 % Generate or Import image
-N=400;% N^2 Pixels in Phantom Image
-imageMatrix = phantom(N);
-% imageMatrix = imread('AlanTuring.jpg');
-% imageMatrix= im2double(imageMatrix(:,:,1));
+% N=400;% N^2 Pixels in Phantom Image
+% imageMatrix = phantom(N);
+imageMatrix = imread('AlanTuring.jpg');
+imageMatrix= im2double(imageMatrix(:,:,1));
 
 % Portion of original image to be removed (/Corrupted)
 ratio = 0.8;
@@ -20,7 +20,7 @@ mu = 2; % Proximal term parameter
 % Loop Stopping Parameters 
 kmax = 10; % Max Iterations for Algorithm 1
 tol1 = 1e-16; % Tolerence Covergence Parameter for Algorithm 1
-tol2 = 1e-6; % Tolerence for Algorithm 2 on alpha k
+tol2 = 1e-4; % Tolerence for Algorithm 2 on alpha k
 
 % Recover Original Image from Corrupted Image
 XRecovered = Algorithm2(XCorrupted,KnownPixels,mu,kmax,tol1,tol2,alpha);
