2981ea5e7fe1a8c8d489bb23834fe1f69f0b0f4a
~bryceshirley <~bryce.shirley@stfc.ac.uk>
2023-06-05 16:17:35 +0100

    Randomized function

--- AlgorithmFunctions/LowRankContribution.m
+++ AlgorithmFunctions/LowRankContribution.m
@@ -1,16 +1,16 @@
 function Uwn = LowRankContribution(Y,Xk,mode_n,lambda1,p,mu)
 % Function used in Algorithm1_3DV2
 % Function takes into account the low-rank contributions of an unfolding
-
+    r=30;
     % Compute Classical SVDs in Unfolding
-    [~,Sn,~] = ComputeSVDUnfoldedxTensor(Xk,mode_n); % mode-n
+    [~,Sn,~] = ComputeRandomizedSVDUnfoldedxTensor(Xk,mode_n,r); % mode-n
 
     % Use the Lp-norm function as a surragate for calculating rank
     wn = Derivative_Weighted_Lpnorm_SF(diag(Sn),lambda1,p)./mu;
 
     % Use Shrinkage Operation on Unfoldings to take into account low-rank
     % properties of the images.
-    [Un, Sn, ~] = ComputeSVDUnfoldedxTensor(Y,mode_n);
+    [Un, Sn, ~] = ComputeRandomizedSVDUnfoldedxTensor(Y,mode_n,r);
     Swn = shrinkage(diag(Sn), wn); 
     ind = find(Swn>0);
     Uwn = Un(:,ind);
