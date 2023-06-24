1f2a6ca82656e4087faf8ac0101ec251344bb2ca
~bryceshirley <~bryce.shirley@stfc.ac.uk>
2023-06-24 16:54:11 +0100

    New spiral branch

--- data_completion.m
+++ data_completion.m
@@ -10,6 +10,44 @@ addpath ('AlgorithmFunctions\','Algorithms\','TensorOperationFunctions\','Tests\
 % Pick DataSet
 [data,file_name] = PickDataSet('DataFiles');
 
+%% Spiral Test
+% Load corresponding full data set:
+full_file_name = ['DataFiles/',file_name(5:17),'_stack_completed.hdf5'];
+complete_data = h5read(full_file_name,'/exchange/data');
+% X_full = reshape(complete_data,n_E,[]);
+
+[n_E,n1,n2]=size(complete_data);
+
+matrix = zeros(n1,n2);
+
+% [sample_matrix,sample_image,ratio]=SpiralSampler(matrix,5,0.08);
+% figure()
+% disp(ratio)
+% imagesc(sample_matrix)
+% 
+% [sample_matrix,sample_image,ratio]=SpiralSampler2(matrix,10,500,0.1);
+% figure()
+% disp(ratio)
+% imagesc(sample_matrix)
+%sample_matrix = reshape(sample_matrix,1,[]);
+
+incomplete_data = reshape(complete_data,n_E,[]);
+sumratio = 0;
+for i=1:n_E
+    data_matrix=incomplete_data(i,:);
+    
+    [sample_matrix,sample_image,ratio]=SpiralSampler(matrix,randi([5,20]),0.05);
+    sumratio = sumratio + ratio;
+    sample_matrix = reshape(sample_matrix,1,[]);
+    data_matrix=data_matrix.*sample_matrix;
+    incomplete_data(i,:,:)=data_matrix;
+end
+disp(sumratio/n_E)
+
+data = reshape(incomplete_data,n_E,n1,n2);
+
+%% End of spiral test
+
 % Format data set:`
 [n_E,n1,n2] = size(data);
 A_sparse = reshape(data,n_E,n1*n2);
